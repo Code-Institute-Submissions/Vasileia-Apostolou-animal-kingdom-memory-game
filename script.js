e22963af8c25acdfc89e10f1dc0ed755c684cc3e
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2020-07-24 14:15:44 +0000

    style 'Click To Start' and 'Game Over' overlay

--- script.js
+++ script.js
@@ -2,11 +2,3 @@ function closeIntroContent() {
     $(".intro-to-game").fadeOut();
 }
 
-function selectedCard(id) {
-console.log("card id", id);
-let cardID = "#card"+id;
-
-if(!$(cardID).hasClass('rotate180')) {
-    $(cardID).addClass('rotate180');
-}
-}
