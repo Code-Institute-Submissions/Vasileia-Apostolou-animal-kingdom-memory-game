93b2432f2d0aeb2389e1b0d66718285d1d191f50
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2020-07-29 10:39:38 +0000

    add JS code for the flipping cards

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
+let animalCards = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D','E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K'];
+
+/*let gameValues = [];
+let memoryIds = [];
+let flippedCards = [];
+
+Array.prototype.gameShuffle = function() {
+    let a = this.length, j,temp;
+
+    while(--a; <0) {
+        j = Math.floor(Math.random() * (i+1));
+        temp = this[j];
+        this[j] = this [a];
+        this[a] = temp;
+    }
+}
+
+function myContainer() {
+    flippedCards = 0;
+    let output= '';
+
+    gameArray.gameShuffle();
+    for (let i=0 i<gameArray.length; i++) {
+        output += ''
+    }
+} */
+
+
+// Hide Intro Overlay 
 function closeIntroContent() {
     $(".intro-to-game").fadeOut();
+}
+
+
+function initialise() {
+    
+    animalCards = shuffle(animalCards);
+
+    for (let i=0; i<10; i++) {
+       let card = "<div class='card'><div class='flip-card' id='card"+i+"' onclick='selectedCard("+i+")'><div class='flip-card-front'></div><div class='flip-card-back'></div></div></div>";
+
+       $(".game-container").append(card);
+    }
 }
 
