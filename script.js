53b6e96252d2c5cb92b7555185eaa96c786ec577
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2020-08-04 20:31:45 +0000

    add images for the front side of the cards

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let animalCards = ['1', '2', '3', '4', '5', '6', '1', '2','3', '4', '5', '6'];
 
 let bgBirdSound = new Audio ("assets/sounds/jungle1.wav");
-let flippingSound = new Audio ("assets/sounds/click1.wav");
+let flippingSound = new Audio ("assets/sounds/click4.mp3");
 let winningSound = new Audio ("assets/sounds/winning-sound.wav");
 let losingSound = new Audio ("assets/sounds/losing-sound.mp3");
+
 
 
 // Hide Intro Overlay 
 function closeIntroContent() {
     $(".intro-to-game").fadeOut();
     initialise();
-    bgBirdSound.play();
-
+    /*bgBirdSound.play();*/
 }
+
+
 
 // Cards
 function initialise() {
@@ -23,9 +25,9 @@ function initialise() {
        let card = "<div class='flip-card'><div class='flip-card-inner' id='card"+i+"' onclick='selectedCard("+i+", "+animalCards[i]+")'><div class='flip-card-front'></div><div class='flip-card-back' style='background-image:url(assets/images/animals/animal"+animalCards[i]+".jpg')></div></div></div>";
 
        $(".game-container").append(card);
+
     }
 }
-
 
 function selectedCard(id) {
     console.log("card id" , id);
@@ -33,19 +35,22 @@ function selectedCard(id) {
 
     if(!$(cardID).hasClass("onclick-cards")) {
         $(cardID).addClass("onclick-cards");
+        flippingSound.play();
     }
 }
 
+
+
+// Shuffle Function
 function shuffle(arra1) {
     var ctr = arra1.length, temp, index;
 
-// While there are elements in the array
     while (ctr > 0) {
-// Pick a random index
+
         index = Math.floor(Math.random() * ctr);
-// Decrease ctr by 1
+
         ctr--;
-// And swap the last element with it
+
         temp = arra1[ctr];
         arra1[ctr] = arra1[index];
         arra1[index] = temp;
