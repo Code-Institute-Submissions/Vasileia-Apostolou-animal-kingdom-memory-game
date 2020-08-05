49c3ab9b6833184c2b135d279226118a41c10e1f
Vasileia-Apostolou <vasiliaapostolou@gmail.com>
2020-08-05 10:48:51 +0000

    add JS code for the countdown timer

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+// Variables
 let animalCards = ['1', '2', '3', '4', '5', '6', '1', '2','3', '4', '5', '6'];
+let flipNumber = 0;
 
+// Variables for sounds
 let bgBirdSound = new Audio ("assets/sounds/jungle1.wav");
 let flippingSound = new Audio ("assets/sounds/click4.mp3");
 let winningSound = new Audio ("assets/sounds/winning-sound.wav");
 let losingSound = new Audio ("assets/sounds/losing-sound.mp3");
+
+// Variables for timer countdown
+let countdown;
+let secondsInput = 60;
+let seconds = secondsInput;
+let secondsLeft = $(".seconds-left");
+
 
 
 
@@ -25,8 +35,8 @@ function initialise() {
        let card = "<div class='flip-card'><div class='flip-card-inner' id='card"+i+"' onclick='selectedCard("+i+", "+animalCards[i]+")'><div class='flip-card-front'></div><div class='flip-card-back' style='background-image:url(assets/images/animals/animal"+animalCards[i]+".jpg')></div></div></div>";
 
        $(".game-container").append(card);
-
     }
+    countdown(seconds);
 }
 
 function selectedCard(id) {
@@ -36,6 +46,12 @@ function selectedCard(id) {
     if(!$(cardID).hasClass("onclick-cards")) {
         $(cardID).addClass("onclick-cards");
         flippingSound.play();
+
+        flipNumber++;
+
+        if(flipNumber ==1){
+            
+        }
     }
 }
 
@@ -56,4 +72,22 @@ function shuffle(arra1) {
         arra1[index] = temp;
     }
     return arra1;
+}
+
+''
+// Countdown Function
+function countdownNow(seconds) {
+   secondsLeft.text(secondsInput);
+
+   if (secondsInput == 0) {
+       clearTimeout(countdown);
+       setTimeout(function() {
+           showResult();
+       }, 1000);
+       return;
+   }
+   secondsInput --;
+   countdown = setTimeout(function() {
+       countdownNow(seconds);
+   },1000);
 }
